52c6b25f61fa4862ca4c3c820cff887aabd3aa21
David3748 <davidel1014.dl@gmail.com>
2025-07-20 16:56:29 -0500

    dallas

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
             <TypeAnimation
               sequence={[
                 2000, // Wait for name animation to finish first
-                'College Park, Maryland', // Type location
+                'Dallas, Texas', // Type location
                 1000, // Wait at final state
               ]}
               wrapper="span"
